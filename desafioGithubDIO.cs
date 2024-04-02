7ab5f75b3e0406e9fe240db737bf494860a195f6
AlailtonNascimento <allayunascimento@gmail.com>
2024-04-01 22:09:16 -0300

    Add README.md

---
+++ desafioGithubDIO.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace projetoDesafioDIO{
+
+    class DesafioGitHub{
+
+        static void Main(String [] args){
+            
+            String nome;
+
+            Console.Write("Oi! Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Bem-Vindo(a)! " + nome);
+
+        }
+    }
+}
